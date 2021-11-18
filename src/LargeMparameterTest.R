3cba6aed3ffc9009fb66d8b07a7fffbf28fa4a60
Postuma <smart-post@hotmail.com>
2021-11-18 11:24:25 +0100

    I did all of this before but did not upload it to github

--- src/LargeMparameterTest.R
+++ src/LargeMparameterTest.R
@@ -1,9 +1,11 @@
 library(ggplot2)
 library(reshape2)
-data<-read.table("output/parameterTest/LargeMparameter.tsv")
+args = commandArgs(trailingOnly=TRUE)
+
+data<-read.table(paste0(args[1],"/LargeMparameter.tsv"))
 data$M<-1:8
 colnames(data)<-c("nLoci","nVariantLoci","coverage","M")
 
 meltedData<-melt(data,id.vars="M")
 ggplot(meltedData,aes(y=value,x=M,col=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")+
-  ggsave("output/parameterTest/LargeMparameter.png")
+  ggsave(paste0(args[1],"/LargeMparameter.png"))
--- src/SmallMparameterTest.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(reshape2)
-data<-read.table("output/parameterTest/SmallMparameter.tsv")
-data$M<-3:7
-colnames(data)<-c("nLoci","nVariantLoci","coverage","M")
-
-meltedData<-melt(data,id.vars="M")
-ggplot(meltedData,aes(y=value,x=M,col=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")+
-  ggsave("output/parameterTest/SmallMParameter.png")
