c8268204c294094b797bf994bb11e126bada3830
Postuma <smart-post@hotmail.com>
2021-11-24 11:23:17 +0100

    Now works according to the New Protocol Paper for Stacks 2
    https://doi.org/10.1101/2021.11.02.466953

--- src/LargeMparameterTest.R
+++ src/LargeMparameterTest.R
@@ -3,9 +3,15 @@ library(reshape2)
 args = commandArgs(trailingOnly=TRUE)
 
 data<-read.table(paste0(args[1],"/LargeMparameter.tsv"))
-data$M<-1:8
+data$M<-1:nrow(data)
 colnames(data)<-c("nLoci","nVariantLoci","coverage","M")
 
 meltedData<-melt(data,id.vars="M")
-ggplot(meltedData,aes(y=value,x=M,col=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")+
+ggplot(meltedData,aes(y=value,x=M,col=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")
   ggsave(paste0(args[1],"/LargeMparameter.png"))
+
+
+diffData<-data.frame(diffVarLoci=diff(data$nLoci),diffLoci=diff(data$nVariantLoci),diffCov=diff(data$coverage),M=2:nrow(data))
+meltDiff<-melt(diffData,id.vars="M")
+ggplot(meltDiff,aes(y=value,x=M,col=variable,group=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")
+  ggsave(paste0(args[1],"LargeMparameterDiff.png"))
