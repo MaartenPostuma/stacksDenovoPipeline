917e2af5b51f46240b42fc202447a35d9d7ae45c
Postuma <smart-post@hotmail.com>
2022-02-10 10:29:53 +0100

    Correct M with data. Correct location of Plots

--- src/LargeMparameterTest.R
+++ src/LargeMparameterTest.R
@@ -3,15 +3,15 @@ library(reshape2)
 args = commandArgs(trailingOnly=TRUE)
 
 data<-read.table(paste0(args[1],"/LargeMparameter.tsv"))
-data$M<-1:nrow(data)
-colnames(data)<-c("nLoci","nVariantLoci","coverage","M")
+data$M<-c(1,10,11,2:9)
+colnames(data)<-c("nVariantLoci","nLoci","coverage","M")
 
 meltedData<-melt(data,id.vars="M")
 ggplot(meltedData,aes(y=value,x=M,col=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")
   ggsave(paste0(args[1],"/LargeMparameter.png"))
 
-
+data<-data[order(data$M),]
 diffData<-data.frame(diffVarLoci=diff(data$nLoci),diffLoci=diff(data$nVariantLoci),diffCov=diff(data$coverage),M=2:nrow(data))
 meltDiff<-melt(diffData,id.vars="M")
 ggplot(meltDiff,aes(y=value,x=M,col=variable,group=variable))+geom_line()+facet_wrap(.~variable,scales="free_y")
-  ggsave(paste0(args[1],"LargeMparameterDiff.png"))
+  ggsave(paste0(args[1],"/LargeMparameterDiff.png"))
